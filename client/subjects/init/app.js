d0cca1dafac976117fb7a9541b3a220b3b4e6276
Raffaele <raffaele.pianella@gmail.com>
2016-06-07 16:23:40 +0000

    subjects init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,22 @@
 // define app module
 // define a run function
+
+var app= angular
+         .module("artooinit",[])
+        .run (() =>{//ECMA SCRIPT 6
+            console.info('I run in the setter!');}); 
+
+
+
+angular
+.module("artooinit")
+.config(function (){})
+.run(function (){})
+.run (() =>{//ECMA SCRIPT 6
+    console.info('I run in the getter!');
+    }); 
+ 
+        
+app.run(function (){
+    console.info('I run in the var!')
+});
